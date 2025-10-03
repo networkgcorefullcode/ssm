c15a271cd8483e6b4c3318351362d7a1ac061a04
PedroVhGit <pedrovh040110@gmail.com>
2025-10-03 17:07:08 -0400

    refactor: Remove unused Swagger UI handler and streamline server setup

--- server/expose_swagger.go
+++ server/expose_swagger.go
@@ -10,6 +10,6 @@ func ServerSwagger() {
 	// Expone la UI de Swagger usando http-swagger
 	http.HandleFunc("/swagger-ui/", httpSwagger.WrapHandler)
 
-	// Inicia el servidor en el puerto 8080
-	http.ListenAndServe(":8080", nil)
+	// Inicia el servidor en el puerto 9001
+	http.ListenAndServe(":9001", nil)
 }
